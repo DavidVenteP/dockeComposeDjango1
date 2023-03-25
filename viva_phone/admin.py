a4a7636880bd3ef0c830e51e46a599066e488a0f
David Venté <davidventepolo@gmail.com>
2023-03-25 13:48:26 -0500

    25-Mar 04 ~ admin.py

--- viva_phone/admin.py
+++ viva_phone/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import feature_item
 # Register your models here.
+@admin.register(feature_item)
+class Feature_ItemsAdmin(admin.ModelAdmin):
+    list_display = ("pk", "title", "description", "icon")
+    search_filter = ("pk", "title", "icon")
+    search_fields = ("pk", "title", "icon")
+    ordering = ("-pk",)
